d96ab1d2040cbb0e813299492b67b58fb2f256c6
Alejandro Santoyo <alejandro.santoyo@surosystem.com>
2018-12-20 16:25:36 -0600

    Fixed styles

--- src/components/MiniDrawer.js
+++ src/components/MiniDrawer.js
@@ -21,152 +21,152 @@ import MailIcon from '@material-ui/icons/Mail';
 const drawerWidth = 240;
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 20,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
 });
 class MiniDrawer extends React.Component {
-  state = {
-    open: false,
-  };
+    state = {
+        open: false,
+    };
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
 
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
 
-  render() {
-    const { classes, theme } = this.props;
-    const { open } = this.state;
-    return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          style={{ backgroundColor: '#7fd6ae' }}
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar style={{ backgroundColor: '#7fd6ae' }} disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <div>
-                {this.props.title}
+    render() {
+        const { classes, theme } = this.props;
+        const { open } = this.state;
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    style={{ backgroundColor: '#7fd6ae' }}
+                    position="fixed"
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar style={{ backgroundColor: '#7fd6ae' }} disableGutters={!open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(classes.menuButton, open && classes.hide)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <div>
+                            {this.props.title}
+                        </div>
+                        <div style={{  justifyContent: 'flex-end', alignItems: 'center', display: 'flex', flex: 1 }}>
+                            {this.props.icon}
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
+                <main
+                    className={classNames(classes.content, {
+                        [classes.contentShift]: open,
+                    })}
+                >
+
+                    {this.props.main}
+                </main>
             </div>
-            <div style={{marginLeft: '900px'}}>
-              {this.props.icon}
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <main
-          className={classNames(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >
-
-          {this.props.main}
-        </main>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 MiniDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles, { withTheme: true })(MiniDrawer);
--- src/pages/BranchOffices.js
+++ src/pages/BranchOffices.js
@@ -11,157 +11,157 @@ import { IconButton } from '@material-ui/core';
 import MiniDrawer from '../components/MiniDrawer';
 
 const styles = {
-  add: {
-    width: '60px',
-    height: '60px',
-    position: 'fixed',
-    bottom: '20px',
-    right: '30px',
-    Zindex: '99',
-    fonTsize: '18px',
-    border: 'none',
-    outline: 'none',
-    backgroundColor: '#ff1744',
-    color: 'white',
-    cursor: 'pointer',
-    padding: '15px',
-    borderRadius: '50%'
-  }
+    add: {
+        width: '60px',
+        height: '60px',
+        position: 'fixed',
+        bottom: '20px',
+        right: '30px',
+        Zindex: '99',
+        fonTsize: '18px',
+        border: 'none',
+        outline: 'none',
+        backgroundColor: '#ff1744',
+        color: 'white',
+        cursor: 'pointer',
+        padding: '15px',
+        borderRadius: '50%'
+    }
 }
 class BranchOffices extends Component {
-  constructor() {
-    super();
-    this.state = { branchOffices: [], open: false, openUpdated: false, currentBranch: '' };
-    this.headers = { 'Authorization': `JWT ${localStorage.getItem('macrofoto:token')}` };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.handleCloseUpdated = this.handleCloseUpdated.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = { branchOffices: [], open: false, openUpdated: false, currentBranch: '' };
+        this.headers = { 'Authorization': `JWT ${localStorage.getItem('macrofoto:token')}` };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleCloseUpdated = this.handleCloseUpdated.bind(this);
+    }
 
-  componentDidMount() {
-    this.getBranchOffices();
-  }
+    componentDidMount() {
+        this.getBranchOffices();
+    }
 
-  handleSubmit() {
-    this.setState({ open: true })
-  }
+    handleSubmit() {
+        this.setState({ open: true })
+    }
 
-  getBranchOffices() {
-    axios.get(`${Configuration.apiServer}/api/v1/admin/branches`, { headers: this.headers })
-      .then(response => {
-        if ('branches' in response.data) {
-          this.setState({ branchOffices: response.data.branches })
-        }
-      }).catch(error => {
-        console.log(error)
-      })
-  }
+    getBranchOffices() {
+        axios.get(`${Configuration.apiServer}/api/v1/admin/branches`, { headers: this.headers })
+            .then(response => {
+                if ('branches' in response.data) {
+                    this.setState({ branchOffices: response.data.branches })
+                }
+            }).catch(error => {
+                console.log(error)
+            })
+    }
 
-  handleClose() {
-    this.setState({ open: false });
-  };
+    handleClose() {
+        this.setState({ open: false });
+    };
 
-  handleCloseUpdated() {
-    this.setState({ openUpdated: false });
-  };
+    handleCloseUpdated() {
+        this.setState({ openUpdated: false });
+    };
 
-  handleOpenEditModal(params) {
-    this.setState({ openUpdated: true, currentBranch: params })
-  }
+    handleOpenEditModal(params) {
+        this.setState({ openUpdated: true, currentBranch: params })
+    }
 
 
-  handleSave(params) {
-    console.log(params)
-    axios.post(`${Configuration.apiServer}/api/v1/admin/branches`, params, { headers: this.headers })
-      .then(response => {
-        console.log(response)
-        if (response.data.status === 'ok') {
-          this.setState({ open: false })
-          this.getBranchOffices();
-          alert("Sea creado una sucursal con exito")
-        }
-      }).catch(error => {
-        console.log(error)
-      })
-  }
+    handleSave(params) {
+        console.log(params)
+        axios.post(`${Configuration.apiServer}/api/v1/admin/branches`, params, { headers: this.headers })
+            .then(response => {
+                console.log(response)
+                if (response.data.status === 'ok') {
+                    this.setState({ open: false })
+                    this.getBranchOffices();
+                    alert("Sea creado una sucursal con exito")
+                }
+            }).catch(error => {
+                console.log(error)
+            })
+    }
 
-  handleUpdate(params) {
+    handleUpdate(params) {
 
-    axios.put(`${Configuration.apiServer}/api/v1/admin/branches/${this.state.currentBranch.id}`, params, { headers: this.headers })
-      .then(response => {
-        console.log(response)
-        if (response.data.status === 'ok') {
-          this.setState({ openUpdated: false })
-          this.getBranchOffices();
-          alert("Sea modificado una sucursal con exito")
-        }
-      }).catch(error => {
-        console.log(error)
-      })
-  }
+        axios.put(`${Configuration.apiServer}/api/v1/admin/branches/${this.state.currentBranch.id}`, params, { headers: this.headers })
+            .then(response => {
+                console.log(response)
+                if (response.data.status === 'ok') {
+                    this.setState({ openUpdated: false })
+                    this.getBranchOffices();
+                    alert("Sea modificado una sucursal con exito")
+                }
+            }).catch(error => {
+                console.log(error)
+            })
+    }
 
-  handleStatus(params) {
-    axios.put(`${Configuration.apiServer}/api/v1/admin/branches/${params.id}/status`, !params.status, { headers: this.headers })
-      .then(response => {
-        if (response.data.status === 'ok') {
-          this.setState({ open: false })
-          this.getBranchOffices();
-          alert("Sea status una sucursal con exito")
-        }
-      }).catch(error => {
-        console.log(error)
-      })
-  }
+    handleStatus(params) {
+        axios.put(`${Configuration.apiServer}/api/v1/admin/branches/${params.id}/status`, !params.status, { headers: this.headers })
+            .then(response => {
+                if (response.data.status === 'ok') {
+                    this.setState({ open: false })
+                    this.getBranchOffices();
+                    alert("Sea status una sucursal con exito")
+                }
+            }).catch(error => {
+                console.log(error)
+            })
+    }
 
-  render() {
+    render() {
 
-    const BranchOffices = this.state.branchOffices.map(branchOffice => {
-      return (
-        <BasicCard
-          key={branchOffice.id}
-          title={branchOffice.name}
-          description={branchOffice.address}
-          icon={<Home style={{ height: '55%', width: '55%', color: 'white' }} />}
-          button={
-            <div class="dropdown">
-              <IconButton type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                <MoreVertIcon />
-              </IconButton>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <button className="dropdown-item" onClick={() => { this.handleOpenEditModal(branchOffice) }} style={{ marginRight: '5px' }}>Editar</button>
-                <button className="dropdown-item" onClick={() => { this.handleStatus(branchOffice) }} style={{ marginRight: '5px' }}>Estatus</button>
-                <button class="dropdown-item" href="#">Eliminar</button>
-              </div>
-            </div>
-          }
-        />
-      );
-    })
-    return (
-      <MiniDrawer
-        title={<img style={{ width: '70%', height: '70%' }} src={require('../images/macrofoto logo .jpeg')} alt={"Logo"}/>}
-        icon={<img style={{ width: '70%', height: '70%', borderRadius: '50%' }} src={require('../images/descarga.jpeg')} alt={"Imagen usuario"} />}
-        main={
-          <div className="container" style={{ marginTop: '7%', marginRight: '6%' }}>
-            <div className="d-flex justify-content-between flex-wrap">
-              {BranchOffices}
-            </div>
-            <br />
-            <br />
-            <div className="d-flex justify-content-end flex-wrap">
-              <button style={styles.add} onClick={this.handleSubmit} >
-                <AddIcon />
-              </button>
-            </div>
-            <ModalBranchOffices save={this.handleSave} open={this.state.open} close={this.state.close} handleClose={this.handleClose} />
-            <ModalBranchOfficesUpdated save={this.handleUpdate} record={this.state.currentBranch} open={this.state.openUpdated} close={this.state.closeUpdated} handleClose={this.handleCloseUpdated} />
-          </div>
-        }
-      />
-    );
-  }
+        const BranchOffices = this.state.branchOffices.map(branchOffice => {
+            return (
+                <BasicCard
+                    key={branchOffice.id}
+                    title={branchOffice.name}
+                    description={branchOffice.address}
+                    icon={<Home style={{ height: '55%', width: '55%', color: 'white' }} />}
+                    button={
+                        <div class="dropdown">
+                            <IconButton type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                <MoreVertIcon />
+                            </IconButton>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <button className="dropdown-item" onClick={() => { this.handleOpenEditModal(branchOffice) }} style={{ marginRight: '5px' }}>Editar</button>
+                                <button className="dropdown-item" onClick={() => { this.handleStatus(branchOffice) }} style={{ marginRight: '5px' }}>Estatus</button>
+                                <button class="dropdown-item" href="#">Eliminar</button>
+                            </div>
+                        </div>
+                    }
+                />
+            );
+        })
+        return (
+            <MiniDrawer
+                title={<img style={{ height: '60px' }} src={require('../images/macrofoto logo .jpeg')} alt={"Logo"} />}
+                icon={<img style={{  height: '50px', marginRight: '32px', borderRadius: '50%' }} src={require('../images/descarga.jpeg')} alt={"Imagen usuario"} />}
+                main={
+                    <div className="container" style={{ marginTop: '7%', marginRight: '6%' }}>
+                        <div className="d-flex justify-content-between flex-wrap">
+                            {BranchOffices}
+                        </div>
+                        <br />
+                        <br />
+                        <div className="d-flex justify-content-end flex-wrap">
+                            <button style={styles.add} onClick={this.handleSubmit} >
+                                <AddIcon />
+                            </button>
+                        </div>
+                        <ModalBranchOffices save={this.handleSave} open={this.state.open} close={this.state.close} handleClose={this.handleClose} />
+                        <ModalBranchOfficesUpdated save={this.handleUpdate} record={this.state.currentBranch} open={this.state.openUpdated} close={this.state.closeUpdated} handleClose={this.handleCloseUpdated} />
+                    </div>
+                }
+            />
+        );
+    }
 }
 
 
